52aa698f15a1abbbfe7a4fe116ac14bd264179d5
yalitzab <yalitzabueno@gmail.com>
2021-01-24 16:19:40 -0800

    create shopping cart

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -5,18 +5,20 @@ const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
-    });
-    // use this to confirm it works!
-    console.log(state);
-    return <Provider value={[state, dispatch]} {...props} />;
-  };
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: '',
+  });
+  // use this to confirm it works!
+  console.log(state);
+  return <Provider value={[state, dispatch]} {...props} />;
+};
 
-  const useStoreContext = () => {
-    return useContext(StoreContext);
-  };
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
 
-  export { StoreProvider, useStoreContext };
+export { StoreProvider, useStoreContext };
